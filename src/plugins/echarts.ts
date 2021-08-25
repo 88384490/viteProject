52620dd5638cb964c27703eeeaa5c19d679f6f62
wb-zy891828 <wb-zy891828@alibaba-inc.com>
2021-08-25 11:03:18 +0800

    添加echarts组件

---
+++ src/plugins/echarts.ts
@@ -0,0 +1,40 @@
+import * as echarts from "echarts/core";
+import { BarChart, BarSeriesOption } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+} from "echarts/components";
+import { CanvasRenderer } from "echarts/renderers";
+import { IBarChartParam, IEchartsItem } from "@/interface";
+
+class Echarts implements IEchartsItem {
+  public _chart: any | undefined;
+  constructor() {
+    this._chart = Echarts.initChart();
+  }
+  private static initChart() {
+    const components: any[] = [TitleComponent, TooltipComponent, GridComponent];
+    const charts: any[] = [BarChart];
+    const installEvent: any[] = [CanvasRenderer];
+    installEvent.concat([...components, ...charts]);
+    echarts.use([
+      TitleComponent,
+      TooltipComponent,
+      GridComponent,
+      BarChart,
+      CanvasRenderer,
+    ]);
+    return echarts;
+  }
+
+  public useBarChart(options: IBarChartParam) {
+    const { el } = options;
+    this._chart.init(el);
+    return Echarts;
+  }
+
+  public setOptions(options: any) {}
+}
+
+export { Echarts };
