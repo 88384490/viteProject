34c3e345c2458be12e92fdcb1b3a36000799170e
corbet_zhou <corbet@sina.com>
2021-09-07 01:21:03 +0800

    feat: 主播表单
    新增页面

---
+++ src/views/Anchor/AnchorForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="anchorForm">
+    <span class="title">新增</span>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+  import MyForm from '/@/components/form/Form.vue'
+</script>
+
+<style scoped>
+
+</style>
